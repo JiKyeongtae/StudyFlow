03210858a883d7e1493a1f5de8c15083bffd750d
JiKyeongtae <mokokala0813@naver.com>
2026-06-14 12:48:11 +0900

    Initial Commit

---
+++ StudyFlow/StudyFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyFlowApp.swift
+//  StudyFlow
+//
+//  Created by 지경태 on 6/14/26.
+//
+
+import SwiftUI
+
+@main
+struct StudyFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
